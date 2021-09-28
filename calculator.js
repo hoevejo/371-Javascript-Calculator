161c19a33bb297476e497ee94c57dcbcad1536da
Jon M. Hoeve <hoevejo@eos26.cis.gvsu.edu>
2021-09-28 16:02:43 -0400

    First Try

--- calculator.js
+++ calculator.js
@@ -23,6 +23,28 @@ function calc(op){
 		console.log(`Current total: ${total}`);
 		askForOp();
 	}
+	else if(op == "-"){
+                readline.question("Enter Value: ", value2);
+                total = total - value2;
+                console.log(`Current total: ${total}`);
+                askForOp();
+        }
+	else if(op == "*"){
+                readline.question("Enter Value: ", value2);
+                total = total * value2;
+                console.log(`Current total: ${total}`);
+                askForOp();
+        }
+	else if(op == "/"){
+                readline.question("Enter Value: ", value2);
+                total = total / value2;
+                console.log(`Current total: ${total}`);
+                askForOp();
+        }
+	else if(op == "q"){
+                console.log("Calculator go night night now");
+        }
+
 
 
 }
