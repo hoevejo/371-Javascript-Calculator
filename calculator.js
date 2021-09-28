d5da0e05cbf97e76089afcab68326b1517891e88
Gabriel J. Armstrong <armstrga@eos03.cis.gvsu.edu>
2021-09-28 19:28:19 -0400

    Added functionality

--- calculator.js
+++ calculator.js
@@ -12,38 +12,50 @@ const readline = require('readline').createInterface({
 });
 
 function askForOp(){
-	readline.question("Enter operation (+-*/, q to quit):", op => {calc(op);});
+	readline.question("Enter operation (+-*/, q to quit):", op => {calc(op)});
 }
 
+askForOp();
+
 var value2;
+
 function calc(op){
 	if(op == "+"){
-		readline.question("Enter Value: ", value2);
-		total = total + value2;
+		readline.question("Enter Value: ", value2 => {
+		total += parseFloat(value2);
 		console.log(`Current total: ${total}`);
-		askForOp();
+			askForOp();
+		});
 	}
 	else if(op == "-"){
-                readline.question("Enter Value: ", value2);
-                total = total - value2;
+                readline.question("Enter Value: ", value2 => {
+                total -= parseFloat(value2);
                 console.log(`Current total: ${total}`);
                 askForOp();
+		});
         }
 	else if(op == "*"){
-                readline.question("Enter Value: ", value2);
-                total = total * value2;
+                readline.question("Enter Value: ", value2 => {
+                total *= parseFloat(value2);
                 console.log(`Current total: ${total}`);
                 askForOp();
+		});
         }
 	else if(op == "/"){
-                readline.question("Enter Value: ", value2);
-                total = total / value2;
+                readline.question("Enter Value: ", value2 => {
+                total /= parseFloat(value2);
                 console.log(`Current total: ${total}`);
                 askForOp();
+		});
         }
 	else if(op == "q"){
                 console.log("Calculator go night night now");
+		readline.close();
         }
+	else {
+		console.log("Invalid operation, please enter +-*/ or q to quit");
+		askForOp();
+	}
 
 
 
