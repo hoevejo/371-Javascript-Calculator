fa0f5fa3f938b66a151c58fa149a27deb27acabe
Gabriel J. Armstrong <armstrga@eos14.cis.gvsu.edu>
2021-09-28 20:48:24 -0400

    Grade Me

--- calculator.js
+++ calculator.js
@@ -1,10 +1,13 @@
 #!/usr/bin/env node
 
-console.log("Welcome to Running-Calc.js");
+console.log("Welcome to Calculator.js\n");
 
 var total = 0.0;
+const validOps = ["+", "-", "*", "/", "q"]
+var op;
+var value2;
 
-console.log(`Current total: ${total}`);
+console.log(`Current total: ${total}\n`);
 
 const readline = require('readline').createInterface({
 	input: process.stdin,
@@ -12,51 +15,61 @@ const readline = require('readline').createInterface({
 });
 
 function askForOp(){
-	readline.question("Enter operation (+-*/, q to quit):", op => {calc(op)});
+	readline.question("Enter operation (+-*/, q to quit): ", input => {op = input;
+	checkValidOp(op);});
+}
+
+function checkValidOp(op){
+	if (!validOps.includes(op)) {
+		console.log("Invalid operator input, please enter +-*/ or q.");
+		askForOp();
+	} else if (op == "q") {
+		console.log("Calculator go night night now");
+		readline.close();
+	} else {
+		enterVal();
+	}
+}
+
+function enterVal() {
+	readline.question("Enter Value: ", val => {value2 = parseFloat(val);
+	if (isNaN(value2)) {
+		console.log("Invalid number");
+		enterVal();
+	} else {
+		calc();
+	}
+	});
 }
 
 askForOp();
 
-var value2;
-
-function calc(op){
+function calc(){
 	if(op == "+"){
-		readline.question("Enter Value: ", value2 => {
-		total += parseFloat(value2);
-		console.log(`Current total: ${total}`);
-			askForOp();
-		});
-	}
-	else if(op == "-"){
-                readline.question("Enter Value: ", value2 => {
-                total -= parseFloat(value2);
-                console.log(`Current total: ${total}`);
-                askForOp();
-		});
-        }
-	else if(op == "*"){
-                readline.question("Enter Value: ", value2 => {
-                total *= parseFloat(value2);
-                console.log(`Current total: ${total}`);
-                askForOp();
-		});
-        }
-	else if(op == "/"){
-                readline.question("Enter Value: ", value2 => {
-                total /= parseFloat(value2);
-                console.log(`Current total: ${total}`);
-                askForOp();
-		});
-        }
-	else if(op == "q"){
-                console.log("Calculator go night night now");
-		readline.close();
-        }
-	else {
-		console.log("Invalid operation, please enter +-*/ or q to quit");
+		total += value2;
+		console.log(`\nCurrent total: ${total}\n`);
 		askForOp();
 	}
-
-
+	else if(op == "-"){
+                total -= value2;
+                console.log(`\nCurrent total: ${total}\n`);
+                askForOp();
+        }
+	else if(op == "*"){
+                total *= value2;
+                console.log(`\nCurrent total: ${total}\n`);
+                askForOp();
+        }
+	else if(op == "/"){
+		if (value2 == 0) {
+			console.log("Can't divide by 0") 
+			askForOp();
+		} else {
+                	total /= value2;
+                	console.log(`\nCurrent total: ${total}\n`);
+                	askForOp();
+                }
+        }
+	
 
 }
